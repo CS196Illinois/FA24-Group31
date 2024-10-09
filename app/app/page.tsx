94ceba057212c4689ada4e8939a1fc2de394c543
Enya Chen <enyac1207@gmail.com>
2024-10-09 00:10:52 -0500

    added new page and header component

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,12 +4,14 @@ import React from "react"
 import { useState } from 'react';
 import { Container, Group, Burger, Button } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-// import { MantineLogo } from '@mantinex/mantine-logo';
 import classes from './HeaderSimple.module.css';
-import { Title } from '@mantine/core';
 import { Text } from '@mantine/core';
 import { createTheme } from '@mantine/core';
 import { MantineProvider } from "@mantine/core";
+import Link from 'next/link';
+import { HeaderSimple } from "./header";
+
+
 
   const theme = createTheme({
   fontSizes: {
@@ -18,70 +20,32 @@ import { MantineProvider } from "@mantine/core";
     md: '1rem',
     lg: '1.25rem',
     xl: '1.9rem', // Adjust this value to your desired size
+    xxl: '2.5rem'
   },
 });
 
-
-
-const links = [
-  { link: '/about', label: 'Our Mission  ' },
-  { link: '/pricing', label: 'About Us  ' },
-  { link: '/learn', label: 'Community  ' },
-];
-
-export function HeaderSimple() {
-  const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
-
-  const items = links.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      data-active={active === link.link || undefined}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-      }}
-    >
-      {link.label}
-    </a>
-  ));
-
-  return (
-    <header className={classes.header}>
-      <Container size="md" className={classes.inner}>
-        <Text size="xl" fw={600} variant="gradient" gradient={{from: 'red', to: 'blue', deg:90}}> Matched.gg </Text>
-        <Group gap={5} visibleFrom="xs">
-          {items}
-        </Group>
-
-        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
-      </Container>
-    </header>
-  );
-}
 
 
 
 
 export default function Home() {
   return (
-    // test 
     <MantineProvider theme={theme}> 
       <div>
-          <HeaderSimple/>
-          <section className={classes.section}>
+        <HeaderSimple />
+        <section className={classes.section}>
           <Container className={classes.content}>
-            <Title order={1} size="h1"> A League of Legends themed matching app. </Title>
-            <p>Find your perfect partner, in-game or in-real-life.</p>
-            <Button className={classes.getStartedButton} mt="xl"> {/* Add a margin-top for spacing */}
-              Get Started
+            <Text size="xxl" fw={550} c="white"> A League of Legends themed matching app.</Text>
+            <Text size="md" fw={500} c="white">Find your perfect partner, in-game or in-real-life.</Text>
+            <Link href="/get-started/get-started"> {/* Adjust the path based on your folder structure */}
+            <Button component={Link} href="/get-started" className={classes.getStartedButton} mt="xl">
+            Get Started
             </Button>
+            </Link>
           </Container>
-        </section>
+        </section>  
       </div>
     </MantineProvider>
-    
   );
 }
+
