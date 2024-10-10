61fbc7d9029fe5441629d9ed0b64ff59dba36e50
adhi-thirumala <adhi.thirumala@gmail.com>
2024-10-10 14:46:26 -0500

    added unit tests to user class, implemented null checking, made null inputs result in empty sets and empty strings

--- Project/backend/java-server/app/build.gradle.kts
+++ Project/backend/java-server/app/build.gradle.kts
@@ -8,7 +8,12 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("org.graalvm.buildtools.native") version "0.10.3"
 }
+version "1.0-SNAPSHOT"
+
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -23,12 +28,19 @@ dependencies {
     implementation(libs.guava)
     implementation("org.xerial:sqlite-jdbc:3.46.1.3")
     implementation("com.google.code.gson:gson:2.11.0")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java { toolchain { languageVersion = JavaLanguageVersion.of(23) } }
 
 application {
-    // Define the main class for the application.
     mainClass = "org.server.Main"
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
 }
