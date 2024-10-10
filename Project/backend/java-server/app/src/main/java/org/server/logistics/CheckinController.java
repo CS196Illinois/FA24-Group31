81b69be94e790aa6a43b97dfe6715ba9b759c3b4
adhi-thirumala <adhi.thirumala@gmail.com>
2024-10-10 15:48:11 -0500

    made the checkin route

--- Project/backend/java-server/app/src/main/java/org/server/logistics/CheckinController.java
+++ Project/backend/java-server/app/src/main/java/org/server/logistics/CheckinController.java
@@ -1,9 +1,11 @@
-package com.server.public;
+package com.server.logistics;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class CheckinController {
+
     @GetMapping("/api/v1/check_in")
     public String checkin() {
         return "Hello, world!";
