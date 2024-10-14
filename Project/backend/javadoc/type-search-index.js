0a6839b65a86d9e60c3de7f41bc78e4df130f856
VINERAJ <VINERAJ@users.noreply.github.com>
2024-10-14 03:21:27 +0000

    Auto generated Javadocs by Actions

--- Project/backend/javadoc/type-search-index.js
+++ Project/backend/javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.server","l":"DBController"},{"p":"org.server","l":"Main"},{"p":"org.server","l":"RiotWebRequest"},{"p":"org.server","l":"User"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.server","l":"CheckinController"},{"p":"org.server","l":"DBController"},{"p":"org.server","l":"Main"},{"p":"org.server","l":"RiotWebRequest"},{"p":"org.server","l":"User"}]
