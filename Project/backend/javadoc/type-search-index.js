64f0482b548864e9555b02f70549a64bf099e13b
VINERAJ <VINERAJ@users.noreply.github.com>
2024-10-14 03:21:27 +0000

    Auto generated Javadocs by Actions

--- Project/backend/javadoc/type-search-index.js
+++ Project/backend/javadoc/type-search-index.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.server.logistics","l":"CheckinController"},{"p":"org.server","l":"DBController"},{"p":"org.server.userops","l":"FetchController"},{"p":"org.server","l":"Main"},{"p":"org.server","l":"RiotWebRequest"},{"p":"org.server.userops","l":"User"},{"p":"org.server","l":"UserController"}]
+=======
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.server","l":"CheckinController"},{"p":"org.server","l":"DBController"},{"p":"org.server","l":"Main"},{"p":"org.server","l":"RiotWebRequest"},{"p":"org.server","l":"User"}]
+>>>>>>> 0a6839b (Auto generated Javadocs by Actions)
