061752153c4ea96605e624108dded0208e9fb16c
adhi-thirumala <adhi-thirumala@users.noreply.github.com>
2024-10-14 02:54:19 +0000

    Auto generated Javadocs by Actions

--- Project/backend/javadoc/package-search-index.js
+++ Project/backend/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.server"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.server"},{"l":"org.server.logistics"}]
