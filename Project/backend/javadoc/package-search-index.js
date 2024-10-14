de4270131e902bdee88249186a5cdcfd18032d68
VINERAJ <VINERAJ@users.noreply.github.com>
2024-10-14 03:21:27 +0000

    Auto generated Javadocs by Actions

--- Project/backend/javadoc/package-search-index.js
+++ Project/backend/javadoc/package-search-index.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.server"},{"l":"org.server.logistics"},{"l":"org.server.userops"}]
+=======
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.server"}]
+>>>>>>> 0a6839b (Auto generated Javadocs by Actions)
