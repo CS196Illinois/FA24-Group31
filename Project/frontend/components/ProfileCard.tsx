8217bc0535840abf494bbd8ea739c22a30640ec9
barry271828-e <barry271828@outlook.com>
2024-10-17 13:02:42 -0500

    replaced tailwind css with separated css files

--- Project/frontend/components/ProfileCard.tsx
+++ Project/frontend/components/ProfileCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classes from './profiles.module.css';
 
 interface ProfileCardProps {
   name: string;
@@ -8,10 +9,10 @@ interface ProfileCardProps {
 
 const ProfileCard: React.FC<ProfileCardProps> = ({ name, image, bio }) => {
   return (
-    <div className="w-500 h-80 bg-white rounded-lg shadow-lg flex flex-col items-center justify-ceter">
-      <img src={image} alt={name} className="w-24 h-24 rounded-full mb-2" />
-      <h2 className="text-2xl font-bold">{name}</h2>
-      <p className="text-gray-600">{bio}</p>
+    <div className={classes.profileCard}>
+      <img src={image} alt={name} className={classes.profileCardImage} />
+      <h2 className={classes.profileCardName}>{name}</h2>
+      <p className={classes.profileCardBio}>{bio}</p>
     </div>
   );
 };
