5a705be5c0fa91e33ac806177ce9eea8d7695b62
mswaminathan <mayasswaminathan@gmail.com>
2024-10-17 18:52:29 -0500

    minor UI changes

--- Project/frontend/app/matching/page.tsx
+++ Project/frontend/app/matching/page.tsx
@@ -26,8 +26,8 @@ const profilesData = [
 const MatchingPage: React.FC = () => {
   return (
     <div className={classes.homepageContainer}>
-      <h1 className={classes.homepageTitle}>Swipe Profiles</h1>
-      <SwipeProfiles profiles={profilesData} />
+        <h1 className={classes.homepageTitle}>Swipe Profiles</h1>
+        <SwipeProfiles profiles={profilesData} />
     </div>
   );
 };
