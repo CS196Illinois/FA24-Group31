46c6eaa6e84687d23e8cca59ca571f868c907b21
inc0gnit0 <iinc0gnit0@pm.me>
2024-10-16 22:16:42 -0500

    removed random files and updated gitignore

--- Project/backend/java-server/app/src/main/java/org/server/UserController.java
+++ Project/backend/java-server/app/src/main/java/org/server/UserController.java
@@ -1,5 +1,6 @@
 package org.server;
 
+<<<<<<< HEAD
 import org.server.userops.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -65,4 +66,42 @@ public class UserController {
             );
         }
     }
+=======
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.server.DBController;
+
+import javax.swing.*;
+import java.sql.*;
+
+@RestController
+public class UserController {
+    private static final String DB_URL = "jdbc:sqlite:main.db";
+
+    DBController db = new DBController(DB_URL);
+    @PostMapping("/api/v1/_delete_user_")
+    public void deleteUser(@PathVariable String uuid) {
+        User user = db.getUser(uuid, true);
+        boolean deleted = db.deleteUser(user);
+        if (deleted) {
+            System.out.println("Deleted user: " + uuid);
+        }
+    }
+    @PostMapping("/api/v1/add_user")
+    public void addUser(@PathVariable String uuid, @PathVariable String firstName, @PathVariable String lastName, @PathVariable String riotID, @PathVariable String discordID, @PathVariable String[] oneWayMatched, @PathVariable String[] twoWayMatched) {
+        User user = new User(
+                uuid,
+                firstName,
+                lastName,
+                riotID,
+                discordID,
+                oneWayMatched,
+                twoWayMatched
+        );
+        db.addUser(user);
+    }
+
+>>>>>>> 413c47f (removed random files and updated gitignore)
 }
