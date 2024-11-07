7752e453606e40dbceb742ebf365ae4caabf1b14
CDS0407 <cywoo2@illinois.edu>
2024-11-07 17:20:43 -0600

    update matching

--- Project/frontend/app/matching/page.tsx
+++ Project/frontend/app/matching/page.tsx
@@ -1,34 +1,8 @@
-"use client";
 
 import React from 'react';
-import SwipeProfiles from '../../components/SwipeProfiles';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-const profilesData = [
-  {
-    name: 'Person A',
-    image: '/images/Person_A.jpg',
-    bio: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-  },
-  {
-    name: 'Person B',
-    image: '/images/Person_B.jpg',
-    bio: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-  },
-  {
-    name: 'Person C',
-    image: '/images/Person_C.jpg',
-    bio: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-  },
-  // Add more profiles as needed
-];
-
->>>>>>> 298c887 (added ProfileCard component)
-=======
+import SwipeProfiles from './components/SwipeProfiles';
 import classes from './style.module.css'; // Importing CSS file
->>>>>>> 57276c2 (replaced tailwind css with separated css files)
+import { HeaderSimple } from '../header';
 
 const profilesData = [
   {
@@ -49,34 +23,15 @@ const profilesData = [
   // Add more profiles as needed
 ];
 
-<<<<<<< HEAD
-
-const profilesData = [
-  {
-    name: 'Person A',
-    image: '/images/Person_A.jpg',
-    bio: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-  },
-  {
-    name: 'Person B',
-    image: '/images/Person_B.jpg',
-    bio: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-  },
-  {
-    name: 'Person C',
-    image: '/images/Person_C.jpg',
-    bio: 'Lorem ipsum odor amet, consectetuer adipiscing elit.',
-  },
-  // Add more profiles as needed
-];
-
-=======
->>>>>>> 57276c2 (replaced tailwind css with separated css files)
 const MatchingPage: React.FC = () => {
   return (
-    <div className={classes.homepageContainer}>
-        <h1 className={classes.homepageTitle}>Swipe Profiles</h1>
-        <SwipeProfiles profiles={profilesData} />
+    <div>
+      <HeaderSimple/>
+    
+      <div className={classes.homepageContainer}>
+          <h1 className={classes.homepageTitle}>Swipe Profiles</h1>
+          <SwipeProfiles profiles={profilesData} />
+      </div>
     </div>
   );
 };
