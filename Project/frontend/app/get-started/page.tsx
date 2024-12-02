0f32702af1a38a725a8fb7e75b1507d7d1e67654
adhi <adhi.thirumala@gmail.com>
2024-12-02 16:27:21 -0600

    made discord button go to the right place

--- Project/frontend/app/get-started/page.tsx
+++ Project/frontend/app/get-started/page.tsx
@@ -1,33 +1,56 @@
-// Get started page
+'use client';
 import React from 'react';
-import { Container, Button, Text } from '@mantine/core';
-import { HeaderSimple } from "../header";
+import {Container, Button, Text} from '@mantine/core';
+import {HeaderSimple} from "../header";
 import Head from 'next/head';
-import Link from 'next/link';
-import classes from '../HeaderSimple.module.css';
-
-
-
+import {useDiscordLogin, DiscordLoginParams} from 'react-discord-login';
+import {FaDiscord} from 'react-icons/fa';
 
 const GetStarted = () => {
-  return (
-    <>
-    <HeaderSimple />
-      <Head>
-        <title>Get Started</title>
-      </Head>
-      <Container size="xl" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh', flexDirection: 'column' }}> 
-        
-        <Text size="xl">Welcome to the Get Started Page!</Text>
-        <Text size="md">This is a blank page for new users.</Text>
+    const discordLoginParams: DiscordLoginParams = {
+        /* this is not an API token im not stupid, its a client ID which is irrelevant unless used with the client secret which is on the backend
+        * equivalent of a public key.*/
+        clientId: '1292671408026943532',
+        redirectUri: 'http://localhost:3000/login',
+        responseType: 'token', // or 'code'
+        scopes: ['identify'],
+    }
+    const {buildUrl, isLoading} = useDiscordLogin(discordLoginParams);
 
-        {/* Dicsord Login Link INSERT HERE*/}
-        {/* <Button component={Link} className={classes.loginButton} mt="xl" justify="center"> Login with Discord </Button> */}
-        
-        {/* Add any other login methods below if needed...*/}
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <HeaderSimple/>
+            <Head>
+                <title>Get Started</title>
+            </Head>
+            <Container size="xl" style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '75vh',
+                flexDirection: 'column'
+            }}>
+                <Text size="xl">Welcome to the Get Started Page!</Text>
+                <Text size="md">This is a blank page for new users.</Text>
+
+                <Button
+                    onClick={() => (window.location.href = buildUrl())}
+                    disabled={isLoading}
+                    leftSection={<FaDiscord/>}
+                    style={{
+                        backgroundColor: '#7289da',
+                        color: 'white',
+                        marginTop: '20px',
+                        padding: '10px 20px',
+                        fontSize: '16px',
+                        fontWeight: 'bold',
+                    }}
+                >
+                    Sign in with Discord
+                </Button>
+            </Container>
+        </>
+    );
 };
 
 export default GetStarted;
